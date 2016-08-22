e7f4acfd9fd9502f3f47ba56858568b022520370
Diego Pedro <dpedro@hexacta.com>
2016-08-22 17:16:25 -0300

    Se actualiza ejemplo

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('librosApp')
-.controller('TodosLosLibrosCtrl',function ($scope, LibrosService) {
-	$scope.libros = LibrosService.todosLosLibros();
+.controller('TodosLosLibrosCtrl',function (LibrosService) {
+	this.libros = LibrosService.todosLosLibros();
 });
   
   
